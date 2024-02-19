b0e1622727d5bb00bdb8ef09b02d0b1a2870044a
ikmrn <ibragimovkmrn@gmail.com>
2024-02-19 18:08:49 +0500

    feat: Implement 'playRound', refactor for clarity

--- js/index.js
+++ js/index.js
@@ -1,118 +1,115 @@
-// // Add valid options
-// const validOptions = ["Rock", "Paper", "Scissors"];
-
-// // Get computer choice, a random value from 0 to 2
-// function getComputerChoice() {
-//   return Math.floor(Math.random() * 3);
-// }
-
-// // Check input for valid type and return capitalized input
-// function checkInput(input) {
-//   if (typeof input !== "string") {
-//     return "Invalid input. Please provide a string.🎃";
-//   }
-
-//   const inputCap = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
-//   if (validOptions.includes(inputCap)) {
-//     return inputCap;
-//   }
-//   return checkInput(prompt("Choose from Rock, Paper, or Scissors.", ""));
-// }
-
-// function getPlayerChoice(n = 0) {
-//   if (n) {
-//     let playerSelection = checkInput(
-//       prompt("Tie, Let's replay the round. Choose your sign!", "")
-//     );
-//     return validOptions.indexOf(playerSelection);
-//   } else {
-//     let playerSelection = checkInput(prompt("Choose your sign!", ""));
-//     return validOptions.indexOf(playerSelection);
-//   }
-// }
-
-// // Play the game
-// function playRound(playerChoice, computerChoice) {
-//   // Calculate game result using the Rock, Paper, Scissors logic.
-//   // The result will be 0 for a tie, 1 for player win, and 2 for computer win.
-//   // Step 1: Calculate the numeric difference between player and computer choices.
-//   // Step 2: Ensure the result is always non-negative by adding 3.
-//   // Step 3: Use modulo 3 to confine the result to 0, 1, or 2.
-//   let gameResult = (playerChoice - computerChoice + 3) % 3;
-
-//   // Determine the winner
-//   if (gameResult === 0) {
-//     return playRound(getPlayerChoice(1), getComputerChoice());
-//   } else if (gameResult === 1) {
-//     return {
-//       message: `You Win! ${validOptions[playerChoice]} beats ${validOptions[computerChoice]}`,
-//       result: 1,
-//     };
-//   } else {
-//     return {
-//       message: `You Lose! ${validOptions[computerChoice]} beats ${validOptions[playerChoice]}`,
-//       result: -1,
-//     };
-//   }
-// }
-
-// function playGame(numberOfGames) {
-//   let gameResult = 0;
-//   for (let i = 0; i < parseInt(numberOfGames); i++) {
-//     // Play one round
-//     let oneRound = playRound(getPlayerChoice(), getComputerChoice());
-
-//     // Retrieve the result and the message
-//     let roundResult = oneRound.result;
-//     let roundMessage = oneRound.message;
-
-//     gameResult += roundResult;
-//     console.log(gameResult);
-//     console.log(roundMessage);
-//   }
-//   console.log(`Overall score: ${gameResult}`);
-// }
-
-// // playGame(5);
-// console.log(34);
-
 document.addEventListener("DOMContentLoaded", () => {
+  /* Select game elements */
   const rulesBtn = document.querySelector(".footer__rules-btn");
   const modal = document.querySelector(".modal");
   const modalCloseIcon = document.querySelector(".modal__icon");
-  const paperBtn = document.querySelector(".game__btn-paper");
-  const scissorsBtn = document.querySelector(".game__btn-scissors");
-  const button = document.querySelector(".game__btn");
+  const buttons = document.querySelectorAll(".game__btn");
+  const game = document.querySelector(".game");
 
-  /* Modal Rules event listeners */
+  /* Calculating the winner of the round depend on the order of validOptions */
+  const validOptions = ["rock", "paper", "scissors"];
+
+  /* Select Result elements */
+  /* General elements */
+  const Result = document.querySelector(".result");
+  const playAgainBtn = document.querySelector(".result__play-btn");
+  const playAgainLi = document.querySelector(".result__play");
+  const resultOutcome = document.querySelector(".result__outcome");
+  const scoreElement = document.getElementById("score");
+  /* Player elements */
+  const resultPlayerImg = document.getElementById("result__img-player");
+  const resultPlayerBtn = document.getElementById("result__btn-player");
+  /* Computer elements */
+  const resultComputerBtn = document.getElementById("result__btn-computer");
+  const resultComputerImg = document.getElementById("result__img-computer");
+  const resultComputerLi = document.getElementById("result__li-computer");
+
+  /* Variables */
+  let countScore = 0;
+  let playerChoice;
+  let computerChoice;
+
+  resultComputerBtn.classList.add(".game__btn-blinking");
+
+  /* Game options event listeners */
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      /* Hide .game and show .result */
+      game.setAttribute("aria-hidden", true);
+      Result.setAttribute("aria-hidden", false);
+
+      /* Get result of the round */
+      playerChoice = validOptions.indexOf(button.id);
+      computerChoice = Math.floor(Math.random() * 3);
+      roundResult = playRound(playerChoice, computerChoice);
+      countScore += roundResult.score;
+
+      resultPlayerBtn.classList.add(`game__btn-${validOptions[playerChoice]}`);
+      resultPlayerImg.src = `images/icon-${validOptions[playerChoice]}.svg`;
+      resultComputerLi.classList.add("result__li-blinking");
+
+      sleep(200).then(() => {
+        resultComputerLi.setAttribute("aria-hidden", false);
+        playAgainLi.setAttribute("aria-hidden", false);
+        resultComputerLi.classList.remove("result__li-blinking");
+        resultComputerImg.src = `images/icon-${validOptions[computerChoice]}.svg`;
+        resultComputerBtn.classList.add(
+          `game__btn-${validOptions[computerChoice]}`
+        );
+        resultOutcome.innerHTML = roundResult.outcome;
+        scoreElement.innerHTML = countScore;
+      });
+    });
+  });
+
+  /* Play again button event listeners */
+  playAgainBtn.addEventListener("click", () => {
+    game.setAttribute("aria-hidden", false);
+    playAgainLi.setAttribute("aria-hidden", true);
+    Result.setAttribute("aria-hidden", true);
+    resultComputerLi.setAttribute("aria-hidden", true);
+    resultPlayerBtn.classList.remove(`game__btn-${validOptions[playerChoice]}`);
+    resultComputerBtn.classList.remove(
+      `game__btn-${validOptions[computerChoice]}`
+    );
+    resultComputerImg.src = "";
+  });
+
+  /* Button mouseup mousedown event listeners */
+  document.addEventListener("mouseup", () => {
+    buttons.forEach((button) => {
+      button.classList.remove("clicked");
+    });
+  });
+
+  buttons.forEach((button) => {
+    button.addEventListener("mousedown", () => {
+      button.classList.add("clicked");
+    });
+  });
+
+  /* Modal event listeners */
   rulesBtn.addEventListener("click", () => {
     modal.setAttribute("aria-hidden", "false");
   });
-
   modalCloseIcon.addEventListener("click", () => {
     modal.setAttribute("aria-hidden", "true");
   });
 
-
-  /* Options event listeners */
-
-  button.addEventListener("click", () => {
-    button.classList.add("clicked"), "mousedown";
-  });
-
-  document.addEventListener("mouseup", () => {
-    button.classList.remove("clicked");
-  });
-
-  const playerChoice = 1;
-
-  const computerChoice = math.floor(Math.random() * 3);
-  // function getComputerChoice() {
-//   return Math.floor(Math.random() * 3);
-// }
-  function roundResults(playerChoice, computerChoice){
-
+  /* Function to play round */
+  function playRound(playerChoice, computerChoice) {
+    let roundResult = (playerChoice - computerChoice + 3) % 3;
+    // Determine the winner
+    if (roundResult === 0) {
+      return { outcome: "draw", score: 0 };
+    } else if (roundResult === 1) {
+      return { outcome: "you won", score: 1 };
+    } else {
+      return { outcome: "you lose", score: -1 };
+    }
   }
 
-
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 });
