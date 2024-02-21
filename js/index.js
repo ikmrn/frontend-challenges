4a7a488e475a955af4a9e39a724a3c8c64db5e49
ikmrn <ibragimovkmrn@gmail.com>
2024-02-21 12:28:30 +0500

    feat: Add event listeners for rating card

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
- 
+document.addEventListener("DOMContentLoaded", () => {
+  const ratingCard = document.querySelector(".rating-card");
+  const thankCard = document.querySelector(".thank-card");
+  const optionsBtns = document.querySelectorAll(".rating-card__options li button");
+  const submitBtn = document.querySelector(".rating-card__submit-btn");
+  let lastClickedRating = null;
+
+  optionsBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      removeClicked();
+      lastClickedRating = btn.innerHTML;
+      btn.classList.add("clicked");
+    });
+  });
+
+  submitBtn.addEventListener("click", () => {
+    console.log(lastClickedRating);
+    ratingCard.setAttribute("aria-hidden", true)
+    thankCard.setAttribute("aria-hidden", false)
+  });
+
+  function removeClicked() {
+    optionsBtns.forEach((button) => {
+      button.classList.remove("clicked");
+    });
+  }
+});
