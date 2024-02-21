5406c4f635f9221fe9500d542605869eb9dd5fc6
ikmrn <ibragimovkmrn@gmail.com>
2024-02-21 13:38:03 +0500

    fix: Prevent form submission if no option selected

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const ratingCard = document.querySelector(".rating-card");
   const thankCard = document.querySelector(".thank-card");
-  const optionsBtns = document.querySelectorAll(".rating-card__options li button");
+  const optionsBtns = document.querySelectorAll(
+    ".rating-card__options li button"
+  );
   const submitBtn = document.querySelector(".rating-card__submit-btn");
+  const selectedOption = document.getElementById("selected-option");
   let lastClickedRating = null;
 
   optionsBtns.forEach((btn) => {
@@ -14,9 +17,14 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   submitBtn.addEventListener("click", () => {
-    console.log(lastClickedRating);
-    ratingCard.setAttribute("aria-hidden", true)
-    thankCard.setAttribute("aria-hidden", false)
+    if (lastClickedRating === null) {
+      // Prevent form submission if no option is chosen
+      event.preventDefault();
+    } else {
+      ratingCard.setAttribute("aria-hidden", true);
+      thankCard.setAttribute("aria-hidden", false);
+      selectedOption.innerHTML = lastClickedRating;
+    }
   });
 
   function removeClicked() {
