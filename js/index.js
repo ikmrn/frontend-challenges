8b3bad49613860576af54c0186665272d338e759
ikmrn <ibragimovkmrn@gmail.com>
2024-03-01 18:36:41 +0500

    feat: Initial commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const navBtn = document.querySelector(".nav__btn");
+
+  navBtn.addEventListener("click", () => {
+    console.log("click");
+  });
+});
