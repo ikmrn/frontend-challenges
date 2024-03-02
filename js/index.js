59e97d8017c828074c615e671824cd30277f8d7e
ikmrn <ibragimovkmrn@gmail.com>
2024-03-02 11:44:54 +0500

    feat: Add event listeners mobile navigation

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const navBtn = document.querySelector(".nav__btn");
+document.addEventListener("DOMContentLoaded", function () {
+  const navList = document.querySelector(".nav__list");
+  const toggleBtn = document.querySelector(".nav__btn");
 
-  navBtn.addEventListener("click", () => {
-    console.log("click");
+  document.addEventListener("click", function (event) {
+    const targetElement = event.target;
+    const isClickBtn = toggleBtn.contains(targetElement);
+    const isClickNav = navList.contains(targetElement);
+    const isLinkClicked = targetElement.tagName === "A";
+    const isHidden = navList.classList.contains("hidden");
+
+    if (isClickBtn) {
+      !isHidden
+        ? navList.classList.add("hidden")
+        : navList.classList.remove("hidden");
+    } else {
+      (!isClickNav || isLinkClicked) && navList.classList.add("hidden");
+    }
   });
 });
