b30a546675dd8b10c526601efb11f3445579fa96
ikmrn <ibragimovkmrn@gmail.com>
2024-03-05 21:42:51 +0500

    feat: Process email submission

--- js/index.js
+++ js/index.js
@@ -1,26 +1,49 @@
-document
-  .getElementById("newsletter-form")
-  .addEventListener("submit", (event) => {
-    const emailInput = document.getElementById("email-input");
-    const emailError = document.getElementById("email-error");
+// Get DOM elements
+const signUpSection = document.getElementById("sign-up-form");
+const successMessage = document.getElementById("success-message");
+const dismiss = document.getElementById("dismiss");
+const userEmail = document.getElementById("user-email");
+const newsletterForm = document.getElementById("newsletter-form");
+const emailInput = document.getElementById("email-input");
+const emailError = document.getElementById("email-error");
 
-    if (!isValidEmail(emailInput.value)) {
-      event.preventDefault();
-      emailError.classList.remove("hidden");
-      emailInput.classList.add("border-red-400", "bg-red-100", "text-red-400");
-      emailInput.classList.remove("border-gray");
-    } else {
-      emailError.classList.add("hidden");
-      emailInput.classList.remove(
-        "border-red-400",
-        "bg-red-100",
-        "text-red-400"
-      );
-      emailInput.classList.add("border-gray");
-    }
-  });
-
+// Function to check if email is valid
 function isValidEmail(email) {
   const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
   return emailRegex.test(email);
 }
+
+// Function to handle form submission
+function handleFormSubmission(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+
+  // Validate email
+  if (!isValidEmail(emailInput.value)) {
+    // Show error message
+    emailError.classList.remove("hidden");
+    emailInput.classList.add("error");
+    emailInput.classList.remove("border-grayCustom");
+  } else {
+    // Hide error message, show success message
+    emailError.classList.add("hidden");
+    emailInput.classList.remove("error");
+    emailInput.classList.add("border-grayCustom");
+    signUpSection.classList.add("hidden");
+    signUpSection.classList.remove("flex");
+    successMessage.classList.add("flex");
+    successMessage.classList.remove("hidden");
+    userEmail.textContent = emailInput.value;
+  }
+}
+
+// Function to dismiss success message
+function dismissSuccessMessage() {
+  successMessage.classList.remove("flex");
+  successMessage.classList.add("hidden");
+  signUpSection.classList.remove("hidden");
+  signUpSection.classList.add("flex");
+}
+
+// Add event listeners
+newsletterForm.addEventListener("submit", handleFormSubmission);
+dismiss.addEventListener("click", dismissSuccessMessage);
